2f5b1311ad8394b34de8fc3ec0c6aba16055b0d9
luzeao <12820712+luzeao@user.noreply.gitee.com>
2023-05-31 20:35:12 +0800

    5.31晚自习

--- 预习_千峰vue/vue3-app/src/views/Detail/index.vue
+++ 预习_千峰vue/vue3-app/src/views/Detail/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="detail">
+    <!-- 头部 -->
     <AppHeader to=".app-header" title="商品详情" left-arrow back></AppHeader>
+    <!-- 商品信息 -->
     <div v-if="detailInfo">
       <div class="box">
         <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
@@ -17,6 +19,7 @@
         </van-row>
       </div>
 
+      <!-- 底部操作栏 -->
       <van-action-bar>
         <van-action-bar-icon icon="chat-o" text="评论" @click="commentHandler" />
         <van-action-bar-icon icon="cart-o" text="购物车" :badge="badge" @click="router.push('/car')" />
@@ -25,9 +28,11 @@
         <van-action-bar-button type="danger" text="立即购买" />
       </van-action-bar>
 
+      <!-- 评论弹出框 -->
       <van-popup v-model:show="show" round closeable position="bottom" :style="{ height: '90%' }">
-        <h3 style="text-align: center;margin-top: 20px;margin-bottom: 10px;">商品评论</h3>
 
+        <h3 style="text-align: center;margin-top: 20px;margin-bottom: 10px;">商品评论</h3>
+        <!-- 评论编辑框 -->
         <van-cell-group inset>
           <van-field v-model="text" rows="5" border autosize type="textarea" maxlength="500" placeholder="请输入留言"
             show-word-limit />
@@ -38,10 +43,16 @@
           </van-cell>
         </van-cell-group>
 
-        <van-cell-group>
-          <van-cell>
+        <!-- 评论展示框 -->
+        <van-cell-group v-if="!getTextList">
+          <van-empty description="暂时没有评价" />
+        </van-cell-group>
 
-          </van-cell>
+        <van-cell-group v-else v-for="item, index in getTextList" :key="index">
+          <van-cell :title="item.time"></van-cell>
+          <span class="span">评分:</span>
+          <van-rate v-model="item.star" readonly />
+          <van-cell :title="item.text"></van-cell>
         </van-cell-group>
 
       </van-popup>
@@ -57,6 +68,7 @@ import { showConfirmDialog } from 'vant';
 
 import { getProDetailAPI } from "@/api/pro"
 import { addCartAPI, getCartListAPI } from '@/api/cart'
+import { orderlistAPI } from '@/api/order';
 
 import { ref, onMounted } from "vue";
 import { storeToRefs } from "pinia";
@@ -77,13 +89,32 @@ const detailInfo = ref<any>(null)
 const { isLogin, userInfo } = storeToRefs(User)
 
 const show = ref<any>(false)
-const star = ref<any>(4)
+const star = ref<any>(0)
 const text = ref<any>('')
 const textList = ref<any>([])
+const getTextList = ref<any>([])
+const getproid = ref<string>(route.query.proid as any)
+const flag = ref<number>(-1)
+
+// 获取所有订单
+const orderlist = async () => {
+  let res = await orderlistAPI({
+    userid: User.userid
+  })
+  flag.value = res.data.findIndex((item: any, index: any) => item.proid == proid.value)
+  console.log(1111111111, flag.value);
+
+}
 
 // 点击评论事件
 const commentHandler = () => {
-  show.value = true
+  if (flag.value != -1) {
+    show.value = true
+    getTextList.value = JSON.parse((sessionStorage.getItem(getproid.value) as any))
+    console.log(getTextList.value);
+  } else {
+    showFailToast('购买商品后才可以评价哦!')
+  }
 }
 
 // 提交评论事件
@@ -96,8 +127,9 @@ const submitText = () => {
     time,
   }
   textList.value.push(msg)
-  sessionStorage.setItem('评论', JSON.stringify(textList.value))
-  star.value = 5
+  sessionStorage.setItem(getproid.value, JSON.stringify(textList.value))
+  commentHandler()
+  star.value = 0
   text.value = ''
 }
 
@@ -149,13 +181,14 @@ const getCartList = async () => {
   let res = await getCartListAPI({
     userid: User.userid
   })
-  badge.value = res.data.length
-
+  badge.value = res.data.length || []
 }
 
 onMounted(() => {
   getDetail()
   getCartList()
+  textList.value = JSON.parse((sessionStorage.getItem(getproid.value) as any)) || []
+  orderlist()
 })
 
 </script>
@@ -171,8 +204,16 @@ onMounted(() => {
   margin-top: 10px;
 }
 
+.span {
+  margin-left: 15px;
+}
+
 .desc {
   font-size: medium;
   margin-top: 5px;
+}
+
+.comment {
+  padding-left: 10px;
 }
 </style>
